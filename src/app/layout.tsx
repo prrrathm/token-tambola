f211c56549764e3a0ebc5b75efa73e318d73d149
prrrathm <prrrathm@gmail.com>
2025-08-11 18:06:48 +0530

    feat: zustand global store + sonner toast setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import AuthProvider from "./providers/auth-provider";
+import AuthProvider from "../providers/auth-provider";
+import { Toaster } from "@/components/ui/sonner";
 
 const geistSans = Geist({
 	variable: "--font-geist-sans",
@@ -27,6 +28,7 @@ export default function RootLayout({
 		<html lang='en'>
 			<body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
 				<AuthProvider>{children}</AuthProvider>
+				<Toaster />
 			</body>
 		</html>
 	);
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import { authOptions } from "@/lib/auth";
 export default async function Home() {
 	try {
 		const session = await getServerSession(authOptions);
-		if (session) {
+		if (!session) {
 			redirect("/dashboard");
 		}
 		return <SignIn />;
